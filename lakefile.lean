614fb564280b320b8d7a2fcfcfa8aad83e2b7d14
Haoyang Shi <shihaoyang0423@outlook.com>
2025-08-13 23:01:42 +0800

    For Goedel-Prover-V2

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 package «FunEq»
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.10.0"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.9.0"
 
 @[default_target]
 lean_lib «FunEq»
